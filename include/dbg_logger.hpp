b618c4f7b74ff7e8f9e27c8aae76d77144f9424e
tomerab <tomer1452002@gmail.com>
2025-01-03 16:24:44 +0200

    refactor

--- include/dbg_logger.hpp
+++ include/dbg_logger.hpp
@@ -12,35 +12,43 @@ static constexpr const char *GREEN = "\033[1m\033[32m";
 static constexpr const char *YELLOW = "\033[1m\033[33m";
 static constexpr const char *RED = "\033[1m\033[31m";
 
-#define GET_FILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+constexpr auto getFileName = [](const char *fName)
+{
+    return strrchr(fName, '/') ? strrchr(fName, '/') + 1 : fName;
+};
 
-#define GET_TIMESTAMP []() {                                         \
-    auto now = std::chrono::system_clock::now();                     \
-    auto time = std::chrono::system_clock::to_time_t(now);           \
-    std::ostringstream os;                                           \
-    os << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S"); \
-    return os.str();                                                 \
-}()
+constexpr auto getTimestamp = []()
+{
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    std::ostringstream os;
+    os << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+    return os.str();
+};
 
 template <typename T>
 void logStub(const char *lvl, const char *fName, uint32_t fLine, T &&msg)
 {
 #ifndef NDEBUG
-    fmt::println("[{}] [{}{}{}] [{}:{}]: {}", GET_TIMESTAMP, GREEN, lvl, RESET, fName, fLine, msg);
+    fmt::println("[{}] [{}{}{}] [{}:{}]: {}", getTimestamp(), GREEN, lvl, RESET, fName, fLine, msg);
 #endif
 }
 
-constexpr auto dbglog_info = [](auto &&msg)
+constexpr auto dbglog_info = [](auto &&msg, const char *fName)
 {
-    logStub("INFO", GET_FILENAME, __LINE__, std::forward<decltype(msg)>(msg));
+    logStub("INFO", getFileName(fName), __LINE__, std::forward<decltype(msg)>(msg));
 };
 
-constexpr auto dbglog_warn = [](auto &&msg)
+constexpr auto dbglog_warn = [](auto &&msg, const char *fName)
 {
-    logStub("WARN", GET_FILENAME, __LINE__, std::forward<decltype(msg)>(msg));
+    logStub("WARN", getFileName(fName), __LINE__, std::forward<decltype(msg)>(msg));
 };
 
-constexpr auto dbglog_error = [](auto &&msg)
+constexpr auto dbglog_error = [](auto &&msg, const char *fName)
 {
-    logStub("ERROR", GET_FILENAME, __LINE__, std::forward<decltype(msg)>(msg));
+    logStub("ERROR", getFileName(fName), __LINE__, std::forward<decltype(msg)>(msg));
 };
+
+#define dbglog_info(msg) dbglog_info(msg, __FILE__)
+#define dbglog_warn(msg) dbglog_warn(msg, __FILE__)
+#define dbglog_error(msg) dbglog_error(msg, __FILE__)
